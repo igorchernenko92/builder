653f49662df9a4ef46316aab49d3ef81d57f792e
Igor Chernenko <igorchernenko92@gmail.com>
2020-06-01 01:18:32 +0300

    ADD: user registration and login

--- wp-content/plugins/sitepoint-upload-master/sitepoint-upload.php
+++ wp-content/plugins/sitepoint-upload-master/sitepoint-upload.php
@@ -85,120 +85,223 @@ function su_load_scripts() {
 add_action('wp_enqueue_scripts', 'su_load_scripts');
 
 
+
+
+
 add_action( 'wp_ajax_login_or_register', 'login_or_register' );
 add_action( 'wp_ajax_nopriv_login_or_register', 'login_or_register' );
 function login_or_register() {
-
-//    require_once get_template_directory() . '/vendor/autoload.php';
-//
-//    $client = new Google_Client(['client_id' => '390155665774-rblk9ocv18mt5npcj79fpufqt5ni5g95.apps.googleusercontent.com']);  // Specify the CLIENT_ID of the app that accesses the backend
-//    $payload = $client->verifyIdToken($_POST['token']);
-//    if ($payload) {
-//        $userid = $payload['sub'];
-//
-//    } else {
-//
+//    if (is_user_logged_in()) { //except of administrator
+//        wp_die();
 //    }
 
-    $main_site = 'test.redcarlos.pro';
+    require_once get_template_directory() . '/vendor/autoload.php';
 
-//    $username = $_POST['name'];
-    $useremail = $_POST['email'];
+    $client = new Google_Client(['client_id' => '390155665774-rblk9ocv18mt5npcj79fpufqt5ni5g95.apps.googleusercontent.com']);  // Specify the CLIENT_ID of the app that accesses the backend
+    $token = $client->verifyIdToken($_POST['token']);
+
+//  TODO: add check if token is got
+    $main_site = 'test.redcarlos.pro';
+    $username = $_POST['name'];
+    $email = $_POST['email'];
     $password = wp_generate_password( 12 );
 
+    if ($token) { //start google token login
+        $gId = $token['sub'];
+
+        $user = reset(get_users(array(  //get user by gId
+                    'meta_key' => '_gToken',
+                    'meta_value' => $gId,
+                    'number' => 1,
+                    'count_total' => false
+                )
+            )
+        );
+
+        if ( $user ) { //user exist, login user
+            $email = $user->data->user_email;
+            $user_id = $user->data->ID;
+
+//            wp_set_current_user($user_id, $email);
+//            wp_set_auth_cookie($user_id);
+//            do_action('wp_login', $email);
+//
+//
+//            $location = 'http://' . get_blogs_of_user($user_id)[1]->domain;
+
+        } else { //if token exist but don't set to user
+
+//
+            if ( $user_id = email_exists($email) ) { //check if email exist
+                if (!is_user_logged_in()) {
+                    add_user_meta($user_id, '_gToken', $gId, true); //set token to email
+                }
+
+//                    wp_set_current_user($user_id, $email); // log in user
+//                    wp_set_auth_cookie($user_id);
+//                    do_action('wp_login', $email);
+//
+//                    $location = 'http://' . get_blogs_of_user($user_id)[1]->domain;
+
+            } else { // if user doesn't exist create user and blog and login user
+
+                $main_site = 'test.redcarlos.pro';
+                $bytes = random_bytes(3); // need for creating unique site name
+                $randName = bin2hex($bytes);     // need for creating unique site name
+                $newdomain = "{$randName}.$main_site"; // create unique domain
+
+                $username = 'user-' . $randName;
+                $password = wp_generate_password( 12 );
+                $email = "email+$randName@example.com";
 
 
-    $rand_number = rand( 1, 2000 );
-    $username = 'user-' . $rand_number;
-    $password = 'fake-password';
-    // $password = wp_generate_password( 12, false );
-    $email = "email+$rand_number@example.com";
-    $user_id = wpmu_create_user( $username, $password, $email );
+                $user_id = wpmu_create_user( $username, $password, $email ); // create network user
+                $user = new WP_User($user_id); // create for adding user role
+                $user->set_role('editor');  //set user new role
 
-    $bytes = random_bytes(3);
+                $blog_id = wpmu_create_blog( $newdomain, '/', $randName, 1 , array( 'public' => 1 ) ); // create blog by admin(id=1)
+                add_user_to_blog($blog_id, $user_id, 'editor');
 
-    $siteName = bin2hex($bytes);
-    $newdomain = "{$siteName}.$main_site";
-    $path = '/';
+//                import_data($blog_id);
+//
+//                $homepage = get_page_by_title( 'first main page' );
+//                if ( $homepage ) {
+//                    update_option( 'page_on_front', $homepage->ID );
+//                    update_option( 'show_on_front', 'page' );
+//                }
 
-    $blog_id = wpmu_create_blog( $newdomain, $path, $siteName, $user_id , array( 'public' => 1 ) );
-    $blog = get_blog_details( array( 'blog_id' => $blog_id ) );
 
-//    echo $blog->siteurl;
-//    switch_to_blog( $blog_id );
-    $location = $siteName . '.' . $main_site;
-//    echo $email . ' ';
-//    echo $blog_id . ' ';
-    echo 'http://' . $location;
 
-    $creds = array();
-    $creds['user_login'] = 'email+1217@example.com';
-    $creds['user_password'] = 'fake-password';
 
-    wp_signon($creds);
+            }
+
+        }
+
+    } else {
+
+        //if token doesn't pass
+        wp_die();
+    }
+//
+    ob_start();
+//    var_dump(get_blogs_of_user($user_id));
+    $result = ob_get_clean();
+    echo $result;
+
+
+
+
+    wp_set_current_user($user_id, $email); // log in user
+    wp_set_auth_cookie($user_id);
+    do_action('wp_login', $email);
+
+    $location = 'http://' . get_blogs_of_user($user_id)[1]->domain;  // send link to front
+
+    wp_die();
+
+}
+
+
+
+function create_random_blog_and_user() {
+    $main_site = 'test.redcarlos.pro';
+    $bytes = random_bytes(3); // need for creating unique site name
+    $randName = bin2hex($bytes);     // need for creating unique site name
+    $newdomain = "{$randName}.$main_site"; // create unique domain
+
+
+
+    $username = 'user-' . $randName;
+    $password = wp_generate_password( 12 );
+    $email = "email+$randName@example.com";
+
+
+    $user_id = wpmu_create_user( $username, $password, $email ); // create network user
+    $user = new WP_User($user_id); // create for adding user role
+    $user->set_role('editor');  //set user new role
+
+    $blog_id = wpmu_create_blog( $newdomain, '/', $randName, 1 , array( 'public' => 1 ) ); // create blog by admin (id=1)
+    add_user_to_blog($blog_id, $user_id, 'editor');
+
+    import_data($blog_id);
+
+    $homepage = get_page_by_title( 'first main page' );
+    if ( $homepage ) {
+        update_option( 'page_on_front', $homepage->ID );
+        update_option( 'show_on_front', 'page' );
+    }
+
+
+
+
+    wp_set_current_user($user_id, $email); // log in user
+    wp_set_auth_cookie($user_id);
+    do_action('wp_login', $email);
+
+    echo 'http://' . $newdomain;  // send link to front
+
+    wp_die();
+}
+
+
+function import_data($blog_id) {
     switch_to_blog( $blog_id );
+
     $import = new WP_Import_Custom();
+    $import->fetch_attachments = true;
 
     $templates = trailingslashit( WP_CONTENT_DIR ) . 'uploads/templates.xml';
     $pages = trailingslashit( WP_CONTENT_DIR ) . 'uploads/pages.xml';
     $property = trailingslashit( WP_CONTENT_DIR ) . 'uploads/property.xml';
     $menu = trailingslashit( WP_CONTENT_DIR ) . 'uploads/menu.xml';
+    $media = trailingslashit( WP_CONTENT_DIR ) . 'uploads/media.xml';
+    $all = trailingslashit( WP_CONTENT_DIR ) . 'uploads/all.xml';
 
+//    prevent outputting
+    ob_start();
     $import->import($templates);
     $import->import($pages);
     $import->import($property);
     $import->import($menu);
+    $import->import($media);
 
-    $homepage = get_page_by_title( 'first main page' );
-    if ( $homepage ) {
-        update_option( 'page_on_front', $homepage->ID );
-        update_option( 'show_on_front', 'page' );
-    }
-
-    $all_conditions = [];
-    $query = new \WP_Query( [
-        'posts_per_page' => -1,
-        'post_type' => 'elementor_library',
-//        'fields' => 'ids',
-        'meta_key' => '_elementor_conditions',
-    ] );
-
-    wp_die();
-
+    //    prevent outputting
+    ob_end_clean();
+//    $import->import($all);
 }
 
 
 
 
-function su_image_submission_cb() {
-    check_ajax_referer('myajax-nonce', 'nonce_code');
-
-    $import = new WP_Import_Custom();
-
-//    ob_start();
-//    var_dump($_FILES['file']);
-//    $result = ob_get_clean();
+//function su_image_submission_cb() {
+//    check_ajax_referer('myajax-nonce', 'nonce_code');
 //
-//    echo $result;
-//    $name = trailingslashit( WP_CONTENT_DIR ) . 'uploads/finalpages.xml';
-//    $menu = trailingslashit( WP_CONTENT_DIR ) . 'uploads/menu.xml';
-    $ae = trailingslashit( WP_CONTENT_DIR ) . 'uploads/ae.xml';
-    $property = trailingslashit( WP_CONTENT_DIR ) . 'uploads/property.xml';
+//    $import = new WP_Import_Custom();
 //
-//    $import->import($name);
-//    $import->import($menu);
-
-    $import->import($ae);
-    $import->import($property);
-
-    $homepage = get_page_by_title( 'first main page' );
-    if ( $homepage ) {
-        update_option( 'page_on_front', $homepage->ID );
-        update_option( 'show_on_front', 'page' );
-    }
-
-    wp_die();
-
-}
-add_action( 'wp_ajax_myajax-submit', 'su_image_submission_cb' );
+////    ob_start();
+////    var_dump($_FILES['file']);
+////    $result = ob_get_clean();
+////
+////    echo $result;
+////    $name = trailingslashit( WP_CONTENT_DIR ) . 'uploads/finalpages.xml';
+////    $menu = trailingslashit( WP_CONTENT_DIR ) . 'uploads/menu.xml';
+//    $ae = trailingslashit( WP_CONTENT_DIR ) . 'uploads/ae.xml';
+//    $property = trailingslashit( WP_CONTENT_DIR ) . 'uploads/property.xml';
+////
+////    $import->import($name);
+////    $import->import($menu);
+//
+//    $import->import($ae);
+//    $import->import($property);
+//
+//    $homepage = get_page_by_title( 'first main page' );
+//    if ( $homepage ) {
+//        update_option( 'page_on_front', $homepage->ID );
+//        update_option( 'show_on_front', 'page' );
+//    }
+//
+//    wp_die();
+//
+//}
+//add_action( 'wp_ajax_myajax-submit', 'su_image_submission_cb' );
 //add_action( 'wp_ajax_nopriv_myajax-submit', 'su_image_submission_cb' );
