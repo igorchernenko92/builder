f72ec706fcb592f2f183d64b4f8bae74982c0d4c
Igor Chernenko <igorchernenko92@gmail.com>
2020-06-01 12:57:16 +0300

    ADD: prevention from google double sign up and redirect

--- wp-content/themes/hello-elementor/page-tpl-home.php
+++ wp-content/themes/hello-elementor/page-tpl-home.php
@@ -1,18 +1,18 @@
 <?php  get_header(); ?>
 
 
-<script src="https://apis.google.com/js/platform.js" async defer></script>
-
-<meta name="google-signin-client_id" content="390155665774-rblk9ocv18mt5npcj79fpufqt5ni5g95.apps.googleusercontent.com">
-
-<div class="g-signin2" data-onsuccess="onSignIn"></div>
+<!--<script src="https://apis.google.com/js/platform.js" async defer></script>-->
+<!---->
+<!--<meta name="google-signin-client_id" content="390155665774-rblk9ocv18mt5npcj79fpufqt5ni5g95.apps.googleusercontent.com">-->
+<!---->
+<!--<div class="g-signin2" data-onsuccess="onSignIn"></div>-->
 
 
 
 <script>
 
-    $( "body" ).on( "click", ".g-signin2", function() {
-        localStorage.setItem('check', '1');
+    jQuery( "body" ).on( "click", ".g-signin2", function() {
+        localStorage.setItem('check', '1'); // check if click was
     });
 
     //var data = {
@@ -37,12 +37,12 @@
     function signOut() {
         var auth2 = gapi.auth2.getAuthInstance();
         auth2.signOut().then(function () {
-            console.log('User signed out.');
+            // console.log('User signed out.');
         });
     }
 
     function onSignIn(googleUser) {
-        if ( localStorage.getItem('check') === '0' ) {
+        if ( localStorage.getItem('check') === '0' ) { // prevent two time function call
             return;
         }
         var id_token = googleUser.getAuthResponse().id_token;
@@ -62,7 +62,7 @@
             data: data,
             success: function (data) {
                 console.log(data);
-                // location.href = data;
+                location.href = data;
             },
         });
 
