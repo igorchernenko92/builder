7dc48fe641c8875674cc352882ae876ae40b3c78
Happy-Child <lazuka_99@mail.ru>
2020-06-26 01:09:34 +0300

    FIX: galleries init

--- wp-content/themes/hello-elementor/includes/elementor/widgets/HelloPropertyGallery/assets/js/gallery-base-script.js
+++ wp-content/themes/hello-elementor/includes/elementor/widgets/HelloPropertyGallery/assets/js/gallery-base-script.js
@@ -154,6 +154,8 @@
 
     $(window).on('elementor/frontend/init', function () {
         elementorFrontend.hooks.addAction('frontend/element_ready/hello_property_gallery.hello_gallery_skin1', HelloGallerySkinScript);
+        elementorFrontend.hooks.addAction('frontend/element_ready/hello_property_gallery.hello_gallery_skin2', HelloGallerySkinScript);
+        elementorFrontend.hooks.addAction('frontend/element_ready/hello_property_gallery.hello_gallery_skin3', HelloGallerySkinScript);
     });
 
 })(jQuery);
