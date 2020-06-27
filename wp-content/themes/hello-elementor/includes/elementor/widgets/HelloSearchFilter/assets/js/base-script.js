b05975609637ebcab7287f14ee04d9351bbb1a71
Happy-Child <lazuka_99@mail.ru>
2020-06-28 00:10:44 +0300

    ADD: add search scripts

--- wp-content/themes/hello-elementor/includes/elementor/widgets/HelloSearchFilter/assets/js/base-script.js
+++ wp-content/themes/hello-elementor/includes/elementor/widgets/HelloSearchFilter/assets/js/base-script.js
@@ -1,7 +1,30 @@
 (function($) {
   const HelloPropertySkinScript = function ($scope, $) {
-
-
+    // Select 2
+    const initSelect2 = function () {
+      const $selectFields = $("select");
+      const svgIcon = '<svg class="select-icon" width="1em" height="1em" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" fill="currentColor" class="css-tdckgx-style-ExpandMore"><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path><path d="M0 0h24v24H0z" fill="none"></path></svg>';
+    
+      // -- Functions BEGIN
+      const initSelect = function () {
+        $selectFields.each(function() {
+          if ($(this).hasClass("select2-hidden-accessible")) return;
+        
+          $(this).wrap("<div class='wrap-select form-control'></div>");
+          const $wrapperCurSelect = $(this).parent();
+          $wrapperCurSelect.append(svgIcon);
+        
+          $(this).select2({
+            dropdownParent: $wrapperCurSelect,
+            dropdownAutoWidth: true,
+            width: 'auto'
+          });
+        });
+      };
+    
+      if ($selectFields.length) initSelect();
+    };
+    initSelect2()
   };
   
   $(window).on('elementor/frontend/init', function () {
