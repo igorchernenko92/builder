97c53d8b08921ea333aced4fa4613fa99731993e
Igor Chernenko <igorchernenko92@gmail.com>
2020-07-02 13:16:58 +0300

    ADD: agent post type

--- wp-content/themes/hello-elementor/functions.php
+++ wp-content/themes/hello-elementor/functions.php
@@ -234,34 +234,57 @@ function register_post_types(){
     register_post_type( 'property', [
         'label'  => null,
         'labels' => [
-            'name'               => 'Properties', // основное название для типа записи
-            'singular_name'      => 'Property', // название для одной записи этого типа
-            'add_new'            => 'Add property', // для добавления новой записи
-            'add_new_item'       => 'Add properties', // заголовка у вновь создаваемой записи в админ-панели.
-            'edit_item'          => 'Edit property', // для редактирования типа записи
-            'new_item'           => 'New property', // текст новой записи
-            'view_item'          => 'Look property', // для просмотра записи этого типа.
-            'search_items'       => 'Search properties', // для поиска по этим типам записи
-            'not_found'          => 'Не найдено', // если в результате поиска ничего не было найдено
-            'not_found_in_trash' => 'Не найдено в корзине', // если не было найдено в корзине
-            'parent_item_colon'  => '', // для родителей (у древовидных типов)
-            'menu_name'          => 'Properties', // название меню
+            'name'               => __( 'Properties', 'builder' ),
+            'singular_name'      => __( 'Property', 'builder' ),
+            'add_new'            => __( 'Add property', 'builder' ),
+            'add_new_item'       => __( 'New property', 'builder' ),
+            'edit_item'          => __( 'Edit property', 'builder' ),
+            'new_item'           => __( 'New property', 'builder' ),
+            'view_item'          => __( 'View property', 'builder' ),
+            'search_items'       => __( 'Search property', 'builder' ),
+            'not_found'          => __( 'Property not found', 'builder' ),
+            'not_found_in_trash' => __( 'Property not found', 'builder' ),
+            'parent_item_colon'  => '',
+            'menu_name'          => __( 'Properties', 'builder' ),
         ],
         'description'         => '',
         'public'              => true,
-        // 'publicly_queryable'  => null, // зависит от public
-        // 'exclude_from_search' => null, // зависит от public
-        // 'show_ui'             => null, // зависит от public
-        // 'show_in_nav_menus'   => null, // зависит от public
         'show_in_menu'        => null, // показывать ли в меню адмнки
-        // 'show_in_admin_bar'   => null, // зависит от show_in_menu
         'show_in_rest'        => null, // добавить в REST API. C WP 4.7
         'rest_base'           => null, // $post_type. C WP 4.7
         'menu_position'       => null,
         'menu_icon'           => null,
-        //'capability_type'   => 'post',
-        //'capabilities'      => 'post', // массив дополнительных прав для этого типа записи
-        //'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
+        'hierarchical'        => false,
+        'supports'            => [ 'title', 'editor', 'thumbnail', 'excerpt' ], // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
+        'taxonomies'          => [],
+        'has_archive'         => false,
+        'rewrite'             => true,
+        'query_var'           => true,
+    ] );
+
+    register_post_type( 'agent', [
+        'label'  => null,
+        'labels' => [
+            'name'               => __( 'Agents', 'builder' ),
+            'singular_name'      => __( 'Agent', 'builder' ),
+            'add_new'            => __( 'Add agent', 'builder' ),
+            'add_new_item'       => __( 'Add agent', 'builder' ),
+            'edit_item'          => __( 'Edit agent', 'builder' ),
+            'new_item'           => __( 'New agent', 'builder' ),
+            'view_item'          => __( 'Look agent', 'builder' ),
+            'search_items'       => __( 'Search agent', 'builder' ),
+            'not_found'          => __( 'Agent not found', 'builder' ),
+            'not_found_in_trash' => __( 'Agent no found', 'builder' ),
+            'parent_item_colon'  => '',
+            'menu_name'          => 'Agents',
+        ],
+        'description'         => '',
+        'public'              => true,
+        'show_in_menu'        => null, // показывать ли в меню адмнки
+        'show_in_rest'        => null, // добавить в REST API. C WP 4.7
+        'rest_base'           => null, // $post_type. C WP 4.7
+        'menu_position'       => null,
+        'menu_icon'           => null,
         'hierarchical'        => false,
         'supports'            => [ 'title', 'editor', 'thumbnail', 'excerpt' ], // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
         'taxonomies'          => [],
