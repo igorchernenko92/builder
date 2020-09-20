e467cdc652a6098bd99c55e4851d5d51a0e3dc24
Igor Chernenko <igorchernenko92@gmail.com>
2020-09-20 21:21:48 +0300

    ADD: check if elementor active

--- wp-content/themes/hello-elementor/functions.php
+++ wp-content/themes/hello-elementor/functions.php
@@ -5,6 +5,27 @@
  * @package HelloElementor
  */
 
+
+//function admin_notice_missing_main_plugin() {
+//
+//    if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+//
+//    $message = sprintf(
+//    /* translators: 1: Plugin Name 2: Elementor */
+//        esc_html__( '"%1$s" requires "%2$s" to be installed and activated.', 'text-domain' ),
+//        '<strong>' . esc_html__( 'Plugin Name', 'text-domain' ) . '</strong>',
+//        '<strong>' . esc_html__( 'Elementor', 'text-domain' ) . '</strong>'
+//    );
+//
+//    printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+//
+//}
+
+if ( ! did_action( 'elementor/loaded' ) ) {
+//    add_action( 'admin_notices', 'admin_notice_missing_main_plugin' );
+    return;
+}
+
 use ElementorPro\Modules\ThemeBuilder\Documents\Theme_Document;
 
 if ( ! defined( 'ABSPATH' ) ) {
