88e7b53c97270e1a80263b2195ac9b2d6d37521b
Egor Lazuka (Home) <lazuka_99@mail.ru>
2020-12-26 18:14:04 +0300

    ADD: styling skin2 for listing gallery

--- themes/hello-elementor/includes/elementor/widgets/HelloPropertySlider/assets/js/base-script.js
+++ themes/hello-elementor/includes/elementor/widgets/HelloPropertySlider/assets/js/base-script.js
@@ -1,6 +1,6 @@
 (function($) {
-  const HelloPropertySkinScript = function ($scope, $) {
-    const $sliders = $(".hl-property-slider");
+  const HelloPropertySkin1Script = function ($scope, $) {
+    const $sliders = $(".hl-property-slider-1");
 
     const defaultOptions = function (params) {
       return {
@@ -35,13 +35,53 @@
       })
     }
   };
-  
+
+  const HelloPropertySkin2Script = function ($scope, $) {
+    const $sliders = $(".hl-property-slider-2");
+
+    const defaultOptions = function (params) {
+      return {
+        speed: 800,
+        ...params,
+      }
+    };
+
+    const initListingGallery1 = function ($slider) {
+      if (!$slider) return;
+
+      const customOptions = {
+        navigation: {
+          nextEl: $slider.find(".hl-property-slider__nav_next"),
+          prevEl: $slider.find(".hl-property-slider__nav_prev")
+        },
+        pagination: {
+          el: $slider.find(".hl-property-slider__pagination"),
+          clickable: true,
+          type: "bullets",
+        },
+        slidesPerView: 1,
+      };
+
+      if (!$slider.find("> .swiper-container").hasClass("swiper-container-initialized")) {
+        new Swiper($slider.find("> .swiper-container"), defaultOptions({
+          ...customOptions,
+        }))
+      }
+    };
+
+    if ($sliders.length) {
+      $sliders.each(function () {
+        initListingGallery1($(this));
+      })
+    }
+  };
+
   $(window).on('elementor/frontend/init', function () {
-    elementorFrontend.hooks.addAction('frontend/element_ready/hello_property_slider.skin1', HelloPropertySkinScript);
-    elementorFrontend.hooks.addAction('frontend/element_ready/hello_property_slider.skin2', HelloPropertySkinScript);
+    elementorFrontend.hooks.addAction('frontend/element_ready/hello_property_slider.skin1', HelloPropertySkin1Script);
+    elementorFrontend.hooks.addAction('frontend/element_ready/hello_property_slider.skin2', HelloPropertySkin2Script);
   });
-  
-  
+
+
 })(jQuery);
 
 
