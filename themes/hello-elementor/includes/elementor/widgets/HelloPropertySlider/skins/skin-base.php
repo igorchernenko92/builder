bcb53bd9a6bff2aab9a8e17607b1a339fbb291fc
Egor Lazuka (Home) <lazuka_99@mail.ru>
2020-12-27 17:28:39 +0300

    ADD: styling skin1 for listing gallery

--- themes/hello-elementor/includes/elementor/widgets/HelloPropertySlider/skins/skin-base.php
+++ themes/hello-elementor/includes/elementor/widgets/HelloPropertySlider/skins/skin-base.php
@@ -50,23 +50,106 @@ abstract class Skin_Base extends Elementor_Skin_Base {
     }
 
     public function render_navs() {
-        ?>
-          <button class='hl-property-slider__nav_prev hl-property-slider__nav'>
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512.008 512.008" style="enable-background:new 0 0 512.008 512.008;" xml:space="preserve">
-              <path style="fill:#000;" d="M501.342,245.185H36.424L210.227,71.404c4.093-4.237,3.975-10.99-0.262-15.083 c-4.134-3.992-10.687-3.992-14.82,0l-192,192c-4.171,4.16-4.179,10.914-0.019,15.085c0.006,0.006,0.013,0.013,0.019,0.019l192,192 c4.093,4.237,10.845,4.354,15.083,0.262c4.237-4.093,4.354-10.845,0.262-15.083c-0.086-0.089-0.173-0.176-0.262-0.262 L36.424,266.519h464.917c5.891,0,10.667-4.776,10.667-10.667S507.233,245.185,501.342,245.185z"/>
-              <path d="M202.675,458.519c-2.831,0.005-5.548-1.115-7.552-3.115l-192-192c-4.164-4.165-4.164-10.917,0-15.083l192-192 c4.237-4.093,10.99-3.975,15.083,0.262c3.992,4.134,3.992,10.687,0,14.82L25.758,255.852L210.206,440.3 c4.171,4.16,4.179,10.914,0.019,15.085C208.224,457.39,205.508,458.518,202.675,458.519z"/>
-              <path d="M501.342,266.519H10.675c-5.891,0-10.667-4.776-10.667-10.667s4.776-10.667,10.667-10.667h490.667 c5.891,0,10.667,4.776,10.667,10.667S507.233,266.519,501.342,266.519z"/>
-            </svg>
-          </button>
+      ?>
+        <button class='hl-property-slider__nav_prev hl-property-slider__nav'>
+          <svg width="14" height="25" viewBox="0 0 14 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12.7357 23.7205L13.5004 22.9611C13.7392 22.7221 13.8707 22.4042 13.8707 22.0644C13.8707 21.7247 13.7392 21.4064 13.5004 21.1674L4.83783 12.5053L13.51 3.83313C13.7488 3.59452 13.8801 3.27619 13.8801 2.93657C13.8801 2.59694 13.7488 2.27843 13.51 2.03963L12.7501 1.28009C12.2559 0.785543 11.4509 0.785543 10.9567 1.28009L0.594392 11.6053C0.355787 11.8439 0.187669 12.1619 0.187669 12.5045L0.187669 12.5085C0.187669 12.8483 0.355976 13.1662 0.594393 13.4048L10.9287 23.7205C11.1673 23.9594 11.4948 24.0906 11.8345 24.091C12.1743 24.091 12.4973 23.9594 12.7357 23.7205Z" fill="#F6F7FB"/>
+          </svg>
+        </button>
 
-          <button class='hl-property-slider__nav_next hl-property-slider__nav'>
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
-              <path style="fill:#000;" d="M511.189,259.954c1.649-3.989,0.731-8.579-2.325-11.627l-192-192 c-4.237-4.093-10.99-3.975-15.083,0.262c-3.992,4.134-3.992,10.687,0,14.82l173.803,173.803H10.667 C4.776,245.213,0,249.989,0,255.88c0,5.891,4.776,10.667,10.667,10.667h464.917L301.803,440.328 c-4.237,4.093-4.355,10.845-0.262,15.083c4.093,4.237,10.845,4.354,15.083,0.262c0.089-0.086,0.176-0.173,0.262-0.262l192-192 C509.872,262.42,510.655,261.246,511.189,259.954z"/>
-              <path d="M309.333,458.546c-5.891,0.011-10.675-4.757-10.686-10.648c-0.005-2.84,1.123-5.565,3.134-7.571L486.251,255.88 L301.781,71.432c-4.093-4.237-3.975-10.99,0.262-15.083c4.134-3.992,10.687-3.992,14.82,0l192,192 c4.164,4.165,4.164,10.917,0,15.083l-192,192C314.865,457.426,312.157,458.546,309.333,458.546z"/>
-              <path d="M501.333,266.546H10.667C4.776,266.546,0,261.771,0,255.88c0-5.891,4.776-10.667,10.667-10.667h490.667 c5.891,0,10.667,4.776,10.667,10.667C512,261.771,507.224,266.546,501.333,266.546z"/>
-            </svg>
-          </button>
-        <?php
+        <button class='hl-property-slider__nav_next hl-property-slider__nav'>
+          <svg width="14" height="25" viewBox="0 0 14 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1.26403 1.27954L0.499398 2.03889C0.260605 2.27787 0.129052 2.59582 0.129052 2.93564C0.129052 3.27526 0.260605 3.59359 0.499398 3.83257L9.16193 12.4947L0.489787 21.1669C0.250994 21.4055 0.119629 21.7238 0.119629 22.0634C0.119629 22.4031 0.250994 22.7216 0.489787 22.9604L1.2497 23.7199C1.74388 24.2145 2.54884 24.2145 3.04301 23.7199L13.4054 13.3947C13.644 13.1561 13.8121 12.8381 13.8121 12.4955L13.8121 12.4915C13.8121 12.1517 13.6438 11.8338 13.4054 11.5952L3.07109 1.27954C2.83249 1.04056 2.50492 0.909378 2.1653 0.909002C1.82548 0.909002 1.50244 1.04056 1.26403 1.27954Z" fill="#F6F7FB"/>
+          </svg>
+        </button>
+      <?php
+    }
+
+    public function render_tags() {
+      ?>
+        <div class="hl-listing-card__wrap-tag hl-listing-card__wrap-tag_left">
+          <a href="#" class="hl-listing-card__tag hl-listing-card__tag_green">Feature</a>
+        </div>
+
+        <div class="hl-listing-card__wrap-tag hl-listing-card__wrap-tag_right">
+          <a href="#" class="hl-listing-card__tag hl-listing-card__tag_blue">For Sale</a>
+        </div>
+      <?php
+    }
+
+    public function render_title($post_id, $post_title) {
+      ?>
+      <a href="<?php echo get_post_permalink($post_id) ?>" class="hl-property-slider__item-title">
+          <?php echo $post_title; ?>
+      </a>
+      <?php
+    }
+
+    public function render_price_status($price) { ?>
+        <div class="hl-listing-wrap-price">
+          <div class="hl-property-slider__item-price">
+              <?php echo $price; ?>
+          </div>
+        </div>
+      <?php
+    }
+
+    public function render_location($location) {
+      ?>
+        <div class="hl-listing-card__location">
+          <i class="fa fa-map-marker hl-listing-card__location-icon"></i>
+          <a class="hl-listing-card__location-text" href="http://localhost/?location=new-york">
+            <?php echo $location; ?>
+          </a>
+        </div>
+      <?php
+    }
+
+    public function render_meta_data($meta_data) {
+      $icons_classes = [
+          "rooms" => "fa-door-open",
+          "bedrooms" => "fa-bed",
+          "bathrooms" => "fa-bath",
+          "area" => "fa-square-root-alt",
+      ];
+      $icons_labels = [
+          "rooms" => "Rooms",
+          "bedrooms" => "Bedrooms",
+          "bathrooms" => "Bath",
+          "area" => "Living Area",
+      ];
+      ?>
+        <ul class="hl-listing-card__info">
+            <?php
+              foreach ($meta_data as $key => $value) { ?>
+                <li class="hl-listing-card__info-item">
+                  <i class="fa fa-fas hl-listing-card__icon hl-listing-card__info-icon <?php echo $icons_classes[$key] ?? ''; ?>"></i>
+                  <span class="hl-listing-card__info-value">
+                      <?php echo $value; ?>
+                      <?php echo $icons_labels[$key] ?? ''; ?>
+                  </span>
+                </li>
+                <?php
+              }
+            ?>
+        </ul>
+      <?php
+    }
+
+    public function render_bottom() {
+      ?>
+        <div class="hl-listing-card__bottom mt-auto">
+          <div class="hl-listing-card__bottom-inner">
+            <a href="http://localhost/?agent=dina-riner" class="hl-listing-card__agent hl-listing-card__bottom-item">
+              <img width="350" height="350" src="http://localhost/wp-content/uploads/2020/07/Artboard-2team.jpg" class="hl-listing-card__agent-img hl-img-responsive wp-post-image" alt="" loading="lazy">                        <span class="hl-listing-card__agent-name">Dina Riner</span>
+            </a>
+            <div class="hl-listing-card__bottom-item hl-listing-card__bottom-item_time">
+              <i class="fa fa-calendar hl-listing-card__icon hl-listing-card__bottom-item-icon"></i>
+              <time class="hl-listing-card__bottom-item-text">5 months ago</time>
+            </div>
+          </div>
+        </div>
+      <?php
     }
 
     public function render_item($post) {
@@ -74,92 +157,51 @@ abstract class Skin_Base extends Elementor_Skin_Base {
         $post_id = $post->ID;
         $post_title = $post->post_title;
         $location = get_field('property_location', $post_id, true)['address'] ?? '';
-        $rooms = get_field('property_rooms', $post_id, true);
-        $bedrooms = get_field('property_bedrooms', $post_id, true);
-        $bathrooms = get_field('property_bath', $post_id, true);
-        $area = get_field('property_living_area', $post_id, true);
+        $price = builder_get_property_price($post_id);
+        $meta_data = [
+            "rooms" => get_field('property_rooms', $post_id, true),
+            "bedrooms" => get_field('property_bedrooms', $post_id, true),
+            "bathrooms" => get_field('property_bath', $post_id, true),
+            "area" => get_field('property_living_area', $post_id, true),
+        ];
 
+        $attr = array(
+            'class' => "hl-property-slider__item-bg",
+        );
+        $thumbnail_url = get_the_post_thumbnail_url( $post_id, 'large', $attr );
 
         ?>
-            <div class="hl-property-slider__item">
+            <div class="hl-property-slider__item" style="background-image: url('<?php echo $thumbnail_url; ?>')">
+              <div class="hl-property-slider__item-block">
+                  <div class="hl-listing-card hl-listing-card_skin-4">
+                      <?php
+                        $this->render_tags();
 
-                <?php
-                $attr = array(
-                    'class' => "hl-property-slider__item-bg",
-                );
-                $thumbnail = get_the_post_thumbnail( $post_id, 'large', $attr );
-                    echo $thumbnail;
-                ?>
-                <div class="hl-property-slider__item-inner">
-                    <div class="hl-property-slider__item-block">
-                        <a href="<?php echo get_post_permalink($post_id) ?>" class="hl-property-slider__item-title">
-                            <?php echo $post_title; ?>
-                        </a>
-                        <?php if ($location) : ?>
-                            <div class="hl-property-slider__item-location">
-                                <i class="fa fa-map-marker-alt hl-property-slider__item-location-icon"></i>
-                                <span class="hl-property-slider__item-location-text">
-                                     <?php echo $location; ?>
-                                </span>
-                            </div>
-                        <?php endif; ?>
+                        if ($post_title) :
+                            $this->render_title($post_id, $post_title);
+                        endif;
 
-                        <ul class="hl-property-slider__item-list">
-                            <li class="hl-property-slider__item-list-item">
-                                <i class="fa fa-fas fa-door-open hl-property-slider__item-list-item-icon"></i>
-                                <div class="hl-property-slider__item-list-item-text" >
-                                    <span><?php echo intval($rooms) ?> </span> <span>Rooms</span>
-                                </div>
-                            </li>
+                        if ($price) :
+                            $this->render_price_status($price);
+                        endif;
 
-                            <li class="hl-property-slider__item-list-item">
-                                <i class="fa fa-fas fa-bed hl-property-slider__item-list-item-icon"></i>
-                                <div class="hl-property-slider__item-list-item-text" >
-                                    <span><?php echo intval($bedrooms) ?> </span> <span>Bedrooms</span>
-                                </div>
-                            </li>
+                        if ($location) :
+                            $this->render_location($location);
+                        endif;
 
-                            <li class="hl-property-slider__item-list-item">
-                                <i class="fa fa-fas fa-bath hl-property-slider__item-list-item-icon"></i>
-                                <div class="hl-property-slider__item-list-item-text" >
-                                    <span><?php echo intval($bathrooms) ?> </span> <span>Bathrooms</span>
-                                </div>
-                            </li>
+                        if ($location) :
+                            $this->render_meta_data($meta_data);
+                        endif;
 
-                            <li class="hl-property-slider__item-list-item">
-                                <i class="fa fa-fas fa-square-root-alt hl-property-slider__item-list-item-icon"></i>
-                                <div class="hl-property-slider__item-list-item-text" >
-                                    <span><?php echo intval($area) ?> </span> <span>sq ft</span>
-                                </div>
-                            </li>
-                        </ul>
-
-                        <div class="hl-property-slider__item-bottom">
-<!--                            <ul class="hl-property-slider__item-rating">-->
-<!--                                <li><i class="fas fa-star"></i></li>-->
-<!--                                <li><i class="fas fa-star"></i></li>-->
-<!--                                <li><i class="fas fa-star"></i></li>-->
-<!--                                <li><i class="fas fa-star-half-alt"></i></li>-->
-<!--                                <li><i class="fas fa-star-half-alt"></i></li>-->
-<!--                            </ul>-->
-
-                            <div class="hl-property-slider__item-price">
-                                <?php  echo builder_get_property_price($post_id); ?>
-<!--                                <span class="hl-property-slider__item-text">-->
-<!--                                    starts from-->
-<!--                                </span>-->
-<!--                                <div class="hl-property-slider__item-price-sum">-->
-<!--                                    $21000-->
-<!--                                </div>-->
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                        $this->render_bottom();
+                      ?>
+                  </div>
+              </div>
             </div>
         <?php
     }
 
-	public function render() {
+	  public function render() {
 
         $args = array(
             'post_type' => 'property',
@@ -171,37 +213,16 @@ abstract class Skin_Base extends Elementor_Skin_Base {
         if ( ! $query->have_posts() ) {
             return;
         }
-
-        $images = [
-                'http://demo.lion-coders.com/html/sarchholm-real-estate-template/images/featured/featured_5.jpg',
-                'http://demo.lion-coders.com/html/sarchholm-real-estate-template/images/featured/featured_5.jpg',
-                'http://demo.lion-coders.com/html/sarchholm-real-estate-template/images/featured/featured_5.jpg',
-                'http://demo.lion-coders.com/html/sarchholm-real-estate-template/images/featured/featured_5.jpg',
-                'http://demo.lion-coders.com/html/sarchholm-real-estate-template/images/featured/featured_5.jpg',
-        ]
-
         ?>
             <div class="hl-property-slider-1">
                 <div class="swiper-container">
                     <div class="swiper-wrapper">
                     <?php
-
-
-                    foreach ( $query->posts as $post ) {
-                        echo '<div class="swiper-slide">';
-                            $this->render_item($post);
-                        echo '</div>';
-                    }
-
-//                        $gallery = [0, 1, 2, 3, 4];
-//                        if ($gallery) {
-//                            foreach ($gallery as $count) { ?>
-<!--                                <div class="swiper-slide">-->
-<!--                                    --><?php //$this->render_item($images[$count]); ?>
-<!--                                </div>-->
-<!--                            --><?php
-//                            }
-//                        }
+                      foreach ( $query->posts as $post ) {
+                          echo '<div class="swiper-slide">';
+                              $this->render_item($post);
+                          echo '</div>';
+                      }
                     ?>
                     </div>
 
